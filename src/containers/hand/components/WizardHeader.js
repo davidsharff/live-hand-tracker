f545c5fe2318720d37ad77c6d6363df452670747
David <davesharff@gmail.com>
2019-08-02 15:58:10 -0500

    Wizard header UI collapsed UI improvements

--- src/containers/hand/components/WizardHeader.js
+++ src/containers/hand/components/WizardHeader.js
@@ -15,74 +15,77 @@ export default function WizardHeader(props) {
   return (
     <Row className="mb-1 mx-0">
       {
-        hand.seats.map((s, i) =>
-          <HeaderItem
-            key={i}
-            isButtonInputMode={hand.buttonSeatIndex === null}
-            onClick={() => hand.buttonSeatIndex === null && handleSetButtonSeatIndex(i)}
-            className="d-flex flex-column justify-content-between"
-            isSelected={selectedSeatIndex === i}
-            isInputtingBoardCards={isInputtingCards}
-          >
-            <Row className={`d-flex flex-row m-0 flex-fill justify-content-${isInputtingCards ? 'center' : 'between'}`}>
-              {
-                s.isActive
-                  ? (
-                    <React.Fragment>
-                        <span>
-                          {
-                            i === hand.heroSeatIndex
-                              ? 'Hero'
-                              : `S${ i + 1 }`
-                          }
-                       </span>
-                      {
-                        hand.buttonSeatIndex !== null && !isInputtingCards &&
-                        <span>
-                            { s.isActive
-                              ? getPositionLabelForSeatIndex(hand, i, s)
-                              : 'Empty'
+        hand.seats.map((s, i) => {
+
+          const lastAction =  _.last(getCurrentActionsForSeat(hand, i));
+          const isHero = i === hand.heroSeatIndex;
+
+          return (
+            <HeaderItem
+              key={i}
+              isButtonInputMode={hand.buttonSeatIndex === null}
+              onClick={() => hand.buttonSeatIndex === null && handleSetButtonSeatIndex(i)}
+              className="d-flex flex-column justify-content-between"
+              isSelected={selectedSeatIndex === i}
+              isInputtingBoardCards={isInputtingCards}
+              isInactive={!s.isActive || (lastAction && lastAction.type === handActionTypes.FOLD)}
+            >
+              <Row className={`d-flex flex-row m-0 flex-fill justify-content-${isInputtingCards ? 'center' : 'between'}`}>
+                {
+                  s.isActive
+                    ? (
+                      <React.Fragment>
+                        {
+                          !isInputtingCards &&
+                          <span>
+                            { isHero ? 'Hero' : `S${ i + 1 }` }
+                          </span>
+                        }
+                        {
+                          hand.buttonSeatIndex !== null &&
+                          <span>
+                            {
+                              s.isActive
+                                ? getPositionLabelForSeatIndex(hand, i, s) + ( isInputtingCards && isHero ? ' (H)' : '')
+                                : 'Empty'
                             }
                           </span>
-                      }
-                    </React.Fragment>
-                  )
-                  : <span>Empty</span>
-              }
-            </Row>
-            {
-              !isInputtingCards &&
-              <ActionRow className="d-flex flex-row justify-content-around m-0 flex-fill align-items-start">
-                {
-                  hand.buttonSeatIndex !== null &&
-                  <div style={{fontSize: '12px'}}>
-                    {
-                      (() => {
-                        const action = _.last(getCurrentActionsForSeat(hand, i));
-
-                        return action && (
-                          <React.Fragment>
-                            <div>{_.capitalize(action.type)}</div>
-                            {
-                              (action.type !== handActionTypes.FOLD && action.type !== handActionTypes.CHECK) &&
-                              <div>${getCurrentAmountInvestedForSeat(hand, i)}</div>
-                            }
-                          </React.Fragment>
-                        );
-                      })()
-                    }
-                  </div>
+                        }
+                      </React.Fragment>
+                    )
+                    : <span>Empty</span>
                 }
-              </ActionRow>
-            }
-          </HeaderItem>
-        )
+              </Row>
+              {
+                !isInputtingCards &&
+                <ActionRow className="d-flex flex-row justify-content-around m-0 flex-fill align-items-start">
+                  {
+                    hand.buttonSeatIndex !== null &&
+                    <div style={{fontSize: '12px'}}>
+                      {
+                        lastAction && (
+                        <React.Fragment>
+                          <div>{_.capitalize(lastAction.type)}</div>
+                          {
+                            (lastAction.type !== handActionTypes.FOLD && lastAction.type !== handActionTypes.CHECK) &&
+                            <div>${getCurrentAmountInvestedForSeat(hand, i)}</div>
+                          }
+                        </React.Fragment>
+                        )
+                      }
+                    </div>
+                  }
+                </ActionRow>
+              }
+            </HeaderItem>
+          );
+        })
       }
     </Row>
   );
 }
 
-const HeaderItem = styled(({ isButtonInputMode, isInputtingBoardCards, isSelected, ...rest }) => <Col {...rest} />)`
+const HeaderItem = styled(({ isButtonInputMode, isInactive, isInputtingBoardCards, isSelected, ...rest }) => <Col {...rest} />)`
   flex-basis: 20%;
   font-size: 12px;
   height: ${p => p.isInputtingBoardCards ? '20px' : '75px'};
@@ -93,6 +96,7 @@ const HeaderItem = styled(({ isButtonInputMode, isInputtingBoardCards, isSelecte
     : 'solid #eee 1px'
   };
   transition: height .25s;
+  background-color: ${p => p.isInactive && '#ccc'};
 `;
 
 const ActionRow = styled(Row)`
