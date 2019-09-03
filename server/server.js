83060f34b4c2d5d3a7f7a2ac8eb0bc366c201afc
David <davesharff@gmail.com>
2019-09-03 06:15:09 -0500

    Fix build dir path

--- server/server.js
+++ server/server.js
@@ -5,17 +5,21 @@ const path = require('path');
 const app = express();
 const cors = require('cors');
 
+const buildPath = path.join(__dirname,'../build');
+
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(buildPath));
 
 app.route('/save/session-and-hand')
   .post(saveSessionAndHand)
 
 // TODO: fix. Not serving up actual build. Only the api end points are working currently.
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/', function (req, res) {
+//   const rootAppPath = path.join(buildPath, 'index.html');
+//   console.log('root: path to index html', rootAppPath);
+//   res.sendFile(rootAppPath);
+// });
 
 const PORT = process.env.PORT || 8080;
 app.listen(console.log('Listening on ' + PORT) || PORT);
