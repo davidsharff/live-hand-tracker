53753728017a6336261199ef8d01d739493b31bc
David <davesharff@gmail.com>
2019-09-22 08:05:21 -0500

    Fix completed hand layout

--- src/containers/hand/components/HandComplete.js
+++ src/containers/hand/components/HandComplete.js
@@ -14,46 +14,45 @@ export default function HandCompleteBody(props) {
 
   // TODO: handle split pot
   return (
-    <div>
-      <Typography variant="h5" style={{ textAlign: 'center'}}>
-        Results
-      </Typography>
-      <Typography variant="subtitle2">
-        Board: { board.join(' ')}
-      </Typography>
-      {
-        // TODO: show board first then each position either has hole cards or mucked.
-        _.sortBy(resultDecoratedPositions, ({ amountWon }) => amountWon > 0 ? -1 : 1).map(p =>
-          <React.Fragment key={p.seatIndex}>
-            <Typography variant="subtitle2" style={{ margin: '2px 0'}}>
-              <span>Seat { p.seatIndex + 1}</span> { p.holeCards.length ? 'showed ' + p.holeCards.join(' ') : 'Mucked'}
-            </Typography>
-            {
-              p.handDescription !== 'Mucked' &&
-              <React.Fragment>
-                <Typography variant="subtitle2">
-                  Seat { p.seatIndex + 1} hand { p.handCards.join(' ') }
-                </Typography>
-                <Typography variant="subtitle2">
-                  Seat { p.seatIndex + 1} has { p.handDescription}
-                </Typography>
-              </React.Fragment>
-            }
-            {
-              p.amountWon > 0 &&
-              <Typography variant="subtitle2">
-                Seat { p.seatIndex + 1} wins ${ p.amountWon}
+    <React.Fragment>
+      <div style={{ alignSelf: 'flex-start', width: '100%' }}>
+        <Typography variant="h5" style={{ textAlign: 'center' }}>
+          Results
+        </Typography>
+        <Typography variant="subtitle2">
+          Board: { board.join(' ')}
+        </Typography>
+        {
+          // TODO: show board first then each position either has hole cards or mucked.
+          _.sortBy(resultDecoratedPositions, ({ amountWon }) => amountWon > 0 ? -1 : 1).map(p =>
+            <div key={p.seatIndex}>
+              <Typography variant="subtitle2" style={{ margin: '2px 0'}}>
+                <span>Seat { p.seatIndex + 1}</span> { p.holeCards.length ? 'showed ' + p.holeCards.join(' ') : 'Mucked'}
               </Typography>
-            }
-          </React.Fragment>
-        )
-      }
+              {
+                p.handDescription !== 'Mucked' &&
+                <React.Fragment>
+                  <Typography variant="subtitle2">
+                    Seat { p.seatIndex + 1} hand { p.handCards.join(' ') }
+                  </Typography>
+                  <Typography variant="subtitle2">
+                    Seat { p.seatIndex + 1} has { p.handDescription}
+                  </Typography>
+                </React.Fragment>
+              }
+              {
+                p.amountWon > 0 &&
+                <Typography variant="subtitle2">
+                  Seat { p.seatIndex + 1} wins ${ p.amountWon}
+                </Typography>
+              }
+            </div>
+          )
+        }
+      </div>
       <Button onClick={onCreateNewHand} variant="contained" color="primary" fullWidth style={{ margin: '20px 0'}}>
         Create New Hand
       </Button>
-      <Button variant="contained" color="secondary" fullWidth disabled>
-        Home
-      </Button>
-    </div>
+    </React.Fragment>
   );
 }
